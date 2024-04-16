fe4fff2502a98c822063302d9d2111734cf3ae37
Collyn Godlewski <129763240+Collynpgodlewski@users.noreply.github.com>
2024-04-16 14:50:35 -0400

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,28 @@ $(document).ready(function(){
     $(".panel.active").click(function() {  	$(this).removeClass("active"); 
 });
 
+})
 $(document).ready(function( $ ) {
     $( ".home" ).first().addClass( "active" );
+
+    $('.dish').hover(
+        function(){
+            var src =  $(this).find('img').toggleClass("hovered");
+            // console.log(src)
+            var winWidth = ($('.dishes').width()) - 300;
+            var winHeight = ($('.dishes')[0].offsetHeight) - 200;
+            
+            randomTop = getRandomNumber(0, winHeight);
+            randomLeft = getRandomNumber(0, winWidth);
+
+            $(this).find('img').css({"top": randomTop +"px", "left": randomLeft +"px", transition : 'opacity 1s ease-in-out'});
+
+            function getRandomNumber(min, max) {
+    
+                return Math.random() * (max - min);
+                  
+              }
+        }
+    )
 });
-})
+
